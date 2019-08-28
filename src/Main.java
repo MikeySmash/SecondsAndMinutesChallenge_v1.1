9fc2f2dd354a6a645ba272331c1d9ebed446405f
Mike Smith <mijuamha@gmail.com>
2019-08-28 08:13:05 +0100

    Added leading "0" to times if less than 10.

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,53 @@
 public class Main {
     public static void main(String[] args) {
 
-        getDurationString(60,50);
-        getDurationString(-1);
+        System.out.println(getDurationString(60, 50));
+        System.out.println( getDurationString(3600));
     }
 
     public static String getDurationString(int minutes, int seconds) {
         if (minutes < 0 || seconds < 0 || seconds > 59) {
-            System.out.println("Invalid value");
-            return "";
+
+            return "Invalid value";
         }
 
         int totalMins = minutes + (seconds / 60);
         int hours = totalMins / 60;
         int remainingMins = totalMins % 60;
-        String theTime = minutes + " mins and " +
-                        seconds + " secs = " +
-                        hours + "h " +
-                        remainingMins + "m " +
-                        seconds + "s";
-        System.out.println(theTime);
+
+        String hoursString = hours + "h";
+
+        if (hours < 10) {
+            hoursString = "0" + hoursString;
+        }
+
+        String minutesString = remainingMins + "m";
+
+        if (remainingMins < 10) {
+            minutesString = "0" + minutesString;
+        }
+
+        String secondsString = seconds + "s";
+
+        if (seconds < 10) {
+            secondsString = "0" + secondsString;
+        }
+
+        String theTime =
+                hoursString + " " +
+                minutesString + " " +
+                secondsString;
         return theTime;
     }
 
     public static String getDurationString(int seconds) {
-        if (seconds < 0){
-            System.out.println("Invalid value");
-            return "";
+        if (seconds < 0) {
+            return "Invalid value";
         }
 
         int mins = seconds / 60;
         int remainingSecs = seconds % 60;
-        String minsAndSecs = seconds + "s = " + mins + "m " + remainingSecs + "s.";
-        System.out.println(minsAndSecs);
+
         return getDurationString(mins, remainingSecs);
     }
 
